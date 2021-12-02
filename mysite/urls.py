fe1acc1dfd0105334800464125848ad136d36f16
Arshia Shafiei <arshiashafiei03@gmail.com>
2021-12-02 17:14:03 +0330

    check login status at /register

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('polls/', include('polls.urls')),
     path('blog/', include('blog.urls')),
--- users/views.py
+++ users/views.py
@@ -4,16 +4,19 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been registred, Now you can log in!')
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('profile')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form, 'title': 'registration'})
+        if request.method == "POST":
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Your account has been registred, Now you can log in!')
+                return redirect('login')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form, 'title': 'registration'})
 
 
 @login_required
