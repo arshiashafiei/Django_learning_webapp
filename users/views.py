e2e572aa9cdc8078f90e8f8071076b5fd6b1a4f2
Arshia Shafiei <arshiashafiei03@gmail.com>
2021-12-02 22:01:10 +0330

    Add Update user Profile and Image and Add Image Resizing

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.user.is_authenticated:
@@ -16,9 +16,27 @@ def register(request):
                 return redirect('login')
         else:
             form = UserRegisterForm()
-        return render(request, 'users/register.html', {'form': form, 'title': 'registration'})
+        context = {
+        'form': form,
+        'title': 'Registration'
+    }
+        return render(request, 'users/register.html', context)
 
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    u_form = UserUpdateForm(request.POST or None, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST or None, request.FILES, instance=request.user.profile)
+
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        messages.success(request, f'Your account has been updated!')
+        return redirect('profile')
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'title': 'Profile'
+    }
+    return render(request, 'users/profile.html', context)
